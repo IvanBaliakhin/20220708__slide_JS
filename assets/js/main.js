9440fb3ab8db6e02281f17de79612eade48b7890
Ivan Baliakhin <ivanbalyakhin@gmail.com>
2022-07-08 20:29:52 +0300

    сделали слайдер с кнопками: play, prev, next

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+const slides = document.querySelectorAll('.slide');
+const pauseBtn = document.querySelector('#pause');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+
+let currentSlide = 0;
+let isPlaying = true;
+let timerID = null;
+let interval = 1000;
+
+const SLIDES_LENGTH = slides.length;
+
+function gotoNth(n) {
+  // current: slide active => slide
+  slides[currentSlide].classList.toggle('active');
+  currentSlide = (n + SLIDES_LENGTH) % SLIDES_LENGTH;
+  // next:    slide => slide active
+  slides[currentSlide].classList.toggle('active');
+}
+
+function gotoPrev() {
+  gotoNth(currentSlide - 1);
+}
+
+function gotoNext() {
+  gotoNth(currentSlide + 1);
+}
+
+function pause() {
+  clearInterval(timerID);
+  isPlaying = false;
+  pauseBtn.innerHTML = 'Play';
+}
+
+function play() {
+  timerID = setInterval(gotoNext, interval);
+  isPlaying = true;
+  pauseBtn.innerHTML = 'Pause';
+}
+
+const pausePlay = () => isPlaying ? pause() : play();
+
+function prev() {
+  gotoPrev();
+  pause();
+}
+
+function next() {
+  gotoNext();
+  pause();
+}
+
+
+
+pauseBtn.addEventListener('click', pausePlay);
+prevBtn.addEventListener('click', prev);
+nextBtn.addEventListener('click', next);
+
+timerID = setInterval(gotoNext, interval);
