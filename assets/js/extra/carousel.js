c610b5a7526fe5f4e218d3957ae980518018dba4
Ivan Baliakhin <ivanbalyakhin@gmail.com>
2022-08-30 19:29:45 +0300

    сократили код, доделали рабуту в классе

--- assets/js/extra/carousel.js
+++ assets/js/extra/carousel.js
@@ -1,29 +1,11 @@
 class Carousel {
-  constructor (object) {
-    const settings = this._initConfig(object);
+  constructor (p) {
+    const s = {...{containerID: '#carousel', slideID: '.slide', interval: 2000, isPlaying: true}, ...p};
 
-    this.container = document.querySelector(settings.containerID);
-    this.slides = this.container.querySelectorAll(settings.slideID);
-    this.interval = settings.interval;
-    this.isPlaying = settings.isPlaying;
-  }
-
-  _initConfig(paramsObject) {
-    const defaultSettings = {
-      containerID: '#carousel',
-      slideID: '.slide',
-      interval: 2000,
-      isPlaying: true
-    };
-
-    if (paramsObject) {
-      defaultSettings.containerID = paramsObject.containerID || defaultSettings.containerID;
-      defaultSettings.slideID = paramsObject.slideID || defaultSettings.slideID;
-      defaultSettings.interval = paramsObject.interval || defaultSettings.interval;
-      defaultSettings.isPlaying = paramsObject.isPlaying || defaultSettings.isPlaying;
-    }
-
-    return defaultSettings;
+    this.container = document.querySelector(s.containerID);
+    this.slides = this.container.querySelectorAll(s.slideID);
+    this.interval = s.interval;
+    this.isPlaying = s.isPlaying;
   }
 
   _initProps() {
@@ -41,7 +23,11 @@ class Carousel {
 
   _initControls() {
     const controls = document.createElement('div');
-    const PAUSE = `<span class="control control-pause" id="pause">${this.FA_PAUSE}</span>`;
+    const PAUSE = `<span class="control control-pause" id="pause">
+                      ${this.isPlaying ? this.FA_PAUSE : this.FA_PLAY}
+                   </span>`;
+                   // <span id="fa-pause-icon">${this.FA_PAUSE}</span> вместо условия два спана (урок 1:55)
+                   // <span id="fa-play-icon">${this.FA_PLAY}</span>
     const PREV = `<span class="control control-prev" id="prev">${this.FA_PREV}</span>`;
     const NEXT = `<span class="control control-next" id="next">${this.FA_NEXT}</span>`;
 
@@ -53,7 +39,20 @@ class Carousel {
     this.pauseBtn = document.querySelector('#pause');
     this.prevBtn = document.querySelector('#prev');
     this.nextBtn = document.querySelector('#next');
+
+    // this.pauseIcon = this.container.querySelector('#fa-pause-icon');
+    // this.playIcon = this.container.querySelector('#fa-play-icon');
+    // this.isPlaying ? this._pauseVisible() : this._playVisible();
   }
+
+  // _pauseVisible(isVisible = true) {
+  //   this.pauseIcon.style.opacity = isVisible ? 1 : 0;
+  //   this.playIcon.style.opacity = !isVisible ? 1 : 0;
+  // }
+
+  // _playVisible() {
+  //   this._pauseVisible(false);
+  // }
 
   _initIndicators() {
     const indicators = document.createElement('div');
@@ -79,6 +78,8 @@ class Carousel {
     this.nextBtn.addEventListener('click', this.next.bind(this));
     this.indicatorContainer.addEventListener('click', this._indicate.bind(this));
     document.addEventListener('keydown', this._pressKey.bind(this));
+    this.container.addEventListener('mouseenter', this._pause.bind(this));
+    this.container.addEventListener('mouseleave', this._play.bind(this));
   }
 
   _pressKey(e) {
@@ -87,7 +88,8 @@ class Carousel {
     if (e.code === this.CODE_SPACE) this.pausePlay();
   }
 
-  _tick() {
+  _tick(flag = true) {
+    if (!flag) return;
     this.timerID = setInterval(() => this._gotoNext(), this.interval);
   }
 
@@ -112,12 +114,12 @@ class Carousel {
   _pause() {
     clearInterval(this.timerID);
     this.isPlaying = false;
-    this.pauseBtn.innerHTML = this.FA_PLAY;
+    this.pauseBtn.innerHTML = this.FA_PLAY; // this._playVisible();
   }
   
   _play() {
     this.isPlaying = true;
-    this.pauseBtn.innerHTML = this.FA_PAUSE;
+    this.pauseBtn.innerHTML = this.FA_PAUSE; // this._pauseVisible();
     this._tick();
   }
   
@@ -149,7 +151,7 @@ class Carousel {
     this._initControls();
     this._initIndicators();
     this._initListeners();
-    this._tick();
+    this._tick(this.isPlaying);
   }
 }
 
